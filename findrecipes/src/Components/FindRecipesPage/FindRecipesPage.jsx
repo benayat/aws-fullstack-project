c1a04c3321f23da06ae5edebe73fb84ed2c9e143
Inna Ilyaev <inna4ilyaev@gmail.com>
2021-04-27 19:57:34 +0300

    27/04/21#2Inna

--- findrecipes/src/Components/FindRecipesPage/FindRecipesPage.jsx
+++ findrecipes/src/Components/FindRecipesPage/FindRecipesPage.jsx
@@ -3,11 +3,14 @@ import axios from 'axios';
 import Input from '../Input/Input';
 import Button from '../Button/Button';
 import RecipeCard from '../RecipeCard/RecipeCard';
+import {Spinner} from '../Spinner/Spinner.components';
 import '../FindRecipesPage/FindRecipeStyle.css';
 
 const FindRecipesPage = () => {
 
-const [data,setData] = useState();
+const [data,setData] = useState(null);
+const [url,setUrl] = useState();
+const [loaderToggle,setLoaderToggle] = useState(false);
 // const [description,setDescription] = useState();
 // const [ingredients,setIngredients] = useState();
 // const [prepStages,setPrepStages] = useState();
@@ -15,8 +18,10 @@ const [data,setData] = useState();
 const getApi= async () => {
     console.log("getApi");
     try{
-        const response = await axios.get();
+        const response = await axios.get('https://bankappinna.herokuapp.com/api/users');
         setData(response);
+        console.log(response);
+        setLoaderToggle(false)
         
        
     }catch(err){
@@ -26,21 +31,32 @@ const getApi= async () => {
 
 
 const clickHandler=()=>{
+    setLoaderToggle(true)
+    setData(null);
     getApi();
 }
 
 const changeHandler = (e)=>{
     console.log(e.target.value);
+    setUrl(e.target.value);
 }
     return (
       <div className="recipesContainer">
         <h1>Recipes from ... </h1>
             <div className="recipeSearch">
+                <label>Paste URL here:</label>
                 <Input change={changeHandler}/>
                 <Button click={clickHandler} content="Search"/>
             </div>
-        <RecipeCard/>
-
+            {
+                (data==null) ? null :<RecipeCard/>
+                
+            }
+            {
+                (loaderToggle) ? <Spinner/> :null
+            }
+                    
+        
     </div>
     );
 };
